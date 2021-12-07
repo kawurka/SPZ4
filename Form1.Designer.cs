e8670a49b229c9cf8ada4f4073258e71679243e6
Mykola Tarasov <mykola.tarasov@nure.ua>
2021-12-07 10:44:18 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace SPZ4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.OutButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(276, 135);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(192, 35);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // OutButton
+            // 
+            this.OutButton.Location = new System.Drawing.Point(278, 200);
+            this.OutButton.Name = "OutButton";
+            this.OutButton.Size = new System.Drawing.Size(189, 40);
+            this.OutButton.TabIndex = 1;
+            this.OutButton.Text = "Out";
+            this.OutButton.UseVisualStyleBackColor = true;
+            this.OutButton.Click += new System.EventHandler(this.OutButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OutButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button OutButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace SPZ4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const int n = 10; //Количество цифр
+        static string FilePath; //Путь файла записи
+        static string OutFilePath; // Путь выходного файла
+
+        private void StartButton_Click(object sender, EventArgs e) //Запись в файл
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.InitialDirectory = Directory.GetCurrentDirectory();
+            openfile.Filter = "txt files (*.txt)|*.txt";
+
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                FilePath = openfile.FileName;
+                using (StreamWriter wr = new StreamWriter(FilePath))
+                {
+                    Random rn = new Random(1) ;
+                    for (int i = 0; i < n; i++)
+                    {
+                        wr.Write(rn.Next(1, 5));
+                        wr.Write(',');
+                        if( i == n - 1)
+                        {
+                            wr.Write(rn.Next(1, 5));
+                        }
+                    }
+                }
+                string text = File.ReadAllText(FilePath);
+                MessageBox.Show(text, "Result");
+            }
+        }
+
+        private void OutButton_Click(object sender, EventArgs e) // Запись исходного файла
+        {
+            if (FilePath != null)
+            {
+                OutFilePath = FilePath.Replace(Path.GetExtension(FilePath), ".out");
+                string text = File.ReadAllText(FilePath);
+                string[] arr = text.Split(',');
+                int count = 1;
+                using (StreamWriter wr = new StreamWriter(OutFilePath))
+                {
+                    for (int i = 0; i < arr.Length - 1; i++)
+                    {
+                        if (arr[i] == arr[i + 1])
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            wr.Write(count);
+                            wr.Write(',');
+                            count = 1;
+                        }
+                        if ( i == arr.Length - 2)
+                        {
+                            wr.Write(count);
+                        }
+                    }
+                }
+            }
+
+            if (OutFilePath != null)
+            {
+                string text = File.ReadAllText(OutFilePath);
+                MessageBox.Show(text, "Result");
+            }
+        }
+    }
+}
